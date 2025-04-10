99a50d224543803db94c441f14dae7708bfb492a
Pruthviraj-y2309 <pruthvirajsyadav05@gmail.com>
2025-04-10 23:43:41 +0530

    Update transmitter.ino

--- transmitter.ino
+++ transmitter.ino
@@ -1,5 +1,6 @@
 //Initial Code
-
+//The main idea of the transmitter is to take the input from the user on serial monitor and then send it to the the receiving module of the part
+//
 
 #include <RH_ASK.h>
 
@@ -13,6 +14,7 @@ RH_ASK driver;
 
 
 void setup() {
+  //To check the module is setup properly
   Serial.begin(9600);
   if(!driver.init())
   {
@@ -24,15 +26,15 @@ void setup() {
 }
 
 void loop() {
-  
+
+  //read the data from serial monotr store it in incomingbyte
   while(Serial.available()==0){}
   int incomingByte = Serial.read();
   Serial.print("Data sent:");
-  Serial.print((char)incomingByte);
-  Serial.print("\n");
+  Serial.println((char)incomingByte); //Print the data to be sent
   //driver.send((uint8_t *)incomingByte, strlen(incomingByte));
-  driver.send((uint8_t *)incomingByte, strlen((uint8_t *)incomingByte));
-  driver.waitPacketSent();
-  delay(10);
+  driver.send((uint8_t *)incomingByte, strlen((uint8_t *)incomingByte));//To send the data the type of data is to be specified and to send the length od string sent
+  driver.waitPacketSent(); //Wait till it is sent
+  delay(10); 
 
 }
